f207323b75aea9ae216bf35f1614b76cbfeb0ce2
Ben Fahrmeier <37186521+NinjaWeedle@users.noreply.github.com>
2023-10-11 13:26:11 -0400

    various changes
    Rework menu and title screen, create icon for shells

--- src/main.c
+++ src/main.c
@@ -11,48 +11,32 @@ void end(void);
 bool step(void);
 void draw(void);
 
-int main(void)
-{
-    bool partial_redraw = false;
-	uint8_t bgColor = 0;
-
-	gfx_Begin(gfx_8bpp);
-	gfx_Blit(gfx_screen);
-	gfx_FillScreen(bgColor);
-    gfx_SetDrawBuffer();
-	gfx_SetColor(0xE0);
-	
-	gfx_SetTextFGColor(0x1E);
-	gfx_SetTextScale(2, 2);
-	gfx_PrintStringXY("Calculator", 103, 20);
-    gfx_PrintStringXY("Street", 103, 55);
-	gfx_PrintStringXY("Racing", 103, 80);
-    gfx_PrintStringXY("III", 110, 120);
-	gfx_SetTextScale(1, 1);
-	gfx_PrintStringXY("Press any key", 80, 150);
-	gfx_PrintStringXY("oxiti8", 200, 200);
+void raceMenu(){
+	gfx_FillScreen(0x2);
 	gfx_BlitBuffer();
-	while(!kb_AnyKey());
+}
+
+void mainMenu(){
 
 		uint8_t selected_option = 0;
-			
 		bool key = false;
-		bgColor = 0x20;
-		gfx_FillScreen(bgColor);
+		gfx_FillScreen(0);
+		gfx_SetTextFGColor(0x0A);
+		gfx_PrintStringXY("WELCOME TO CALCULATOR STREET RACING III", 0, 0);
+		gfx_SetTextFGColor(0xDF);
 		gfx_SetTextScale(1, 1);
 		gfx_PrintStringXY("Go Race", 20, 50);
-		gfx_PrintStringXY("Something 2", 20, 60);
-		gfx_PrintStringXY("Also", 20, 70);
-		gfx_PrintStringXY("yes", 20, 80);
-		gfx_PrintStringXY("more words", 20, 90);
-		gfx_PrintStringXY("Not a Car", 20, 100);
-		gfx_PrintStringXY("Quit", 20, 110);
-		gfx_BlitBuffer();
-		gfx_SetColor(bgColor);
+		gfx_PrintStringXY("Change Car", 20, 70);
+		gfx_PrintStringXY("Tuning", 20, 90);
+		gfx_PrintStringXY("Buy Cars", 20, 110);
+		gfx_PrintStringXY("Options", 20, 130);
+		gfx_PrintStringXY("About", 20, 150);
+		gfx_PrintStringXY("Quit Game", 20, 170);
+		gfx_SetColor(0);
 		while(!key) {
 			kb_Scan();
-			key = kb_Data[1] == kb_2nd;
-			gfx_FillRectangle(10, 50 + selected_option*10, 10, 10);
+			key = (kb_Data[1] == kb_2nd);
+			gfx_FillRectangle(10, 50 + selected_option*20, 10, 10);
 			
 			if(kb_Data[7] & kb_Up) {
 				selected_option--;
@@ -63,40 +47,62 @@ int main(void)
 				if(selected_option == 7) selected_option = 0;
 			}
 			
-			gfx_PrintStringXY(">", 10, 50 + selected_option*10);
-			
-			if(selected_option == 0) {
-			}
-			
-			if(selected_option == 1) {
-			}
-			
-			if(selected_option == 2) {
-
-			}
-
-			if(selected_option == 3) {
-
-			}
-
-			if(selected_option == 4) {
-			}
-				
-			if(selected_option == 5) {
-			}
-			
-			if(selected_option == 6) {
-
-			}		
-
+			gfx_PrintStringXY(">", 10, 50 + selected_option*20);
 			gfx_BlitBuffer();
 			delay(100);
+		}
+			switch(selected_option)
+			{
+				case 0:
+					raceMenu();
+					break;
+				case 1:
+					break;
+			
+				case 2:
+					break;
+
+				case 3:
+					break;
+
+				case 4:
+					break;
+				
+				case 5:
+					break;
+			
+				case 6:
+					break;
+			}	
+
+			gfx_BlitBuffer();
 			}
 
-    /* Draw to the buffer to avoid rendering artifacts */
+int main(void)
+{
+    bool partial_redraw = false;
+
+	gfx_Begin(gfx_8bpp);
+	gfx_Blit(gfx_screen);
+	gfx_FillScreen(0x00);
+    gfx_SetDrawBuffer();
+	gfx_SetColor(0xE0);
+	gfx_SetTextFGColor(0x00);
+	gfx_SetTextScale(1, 1);
+	gfx_PrintStringXY("Calculator Street Racing III:", 20, 20);
+		gfx_SetTextFGColor(0x1E);
+    gfx_PrintStringXY("Racing Redefined", 80, 40);
+	gfx_SetTextScale(1, 1);
+	gfx_PrintStringXY("Press any key", 100, 150);
+	gfx_PrintStringXY("oxiti8", 200, 200);
+	gfx_BlitBuffer();
+	while(!kb_AnyKey());
+	mainMenu();
+
     gfx_SetDrawBuffer();
 
     gfx_End();
 
     return 0;
 }
+
